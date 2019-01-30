ea3c7b3bcaf5aa6f6671e646d03071e99be2c130
Lukasz Karpik <lkarpik@wp.pl>
2019-01-30 22:36:47 +0100

    Added config.php file and populated it with array of credentials. Changed Connect class to use config.php

--- Database/Connection.php
+++ Database/Connection.php
@@ -5,8 +5,6 @@ class Connection
     public static function connect($config)
     {
         try {
-            return new PDO("mysql:host=mysql:3306;dbname=todo;charset=utf8", "root", "rootpassword");
-
             return new PDO(
                 $config['connection'].';dbname='.$config['options']['dbname'].';charset='.$config['options']['charset'], $config['username'], $config['rootpassword']
             );
